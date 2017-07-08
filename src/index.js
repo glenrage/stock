c56331754701ed063f25375e703714b5acfe40fa
galang pham <phamgalang@gmail.com>
2017-07-08 15:29:14 -0700

    cleaneup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import InstaTrade from './insta-trade';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<InstaTrade />, document.getElementById('root'));
 registerServiceWorker();
