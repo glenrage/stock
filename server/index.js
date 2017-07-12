0bbd693c39fe37590ae87d3cba23bf4f2168c849
galang pham <phamgalang@gmail.com>
2017-07-12 13:29:39 -0700

    redeploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../client/build')));
